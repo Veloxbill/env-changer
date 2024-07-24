085973363f3070b55a453d2fc967954e66e48b81
SiFuMax09 <131544883+SiFuMax09@users.noreply.github.com>
2024-07-25 00:14:55 +0200

    Update LaravelEnvServiceProvider.php

--- src/LaravelEnvServiceProvider.php
+++ src/LaravelEnvServiceProvider.php
@@ -3,10 +3,6 @@
 namespace veloxbill\LaravelEnv;
 
 use Illuminate\Support\ServiceProvider;
-use msztorc\LaravelEnv\Commands\EnvDelCommand;
-use msztorc\LaravelEnv\Commands\EnvGetCommand;
-use msztorc\LaravelEnv\Commands\EnvListCommand;
-use msztorc\LaravelEnv\Commands\EnvSetCommand;
 
 class LaravelEnvServiceProvider extends ServiceProvider
 {
@@ -23,16 +19,6 @@ class LaravelEnvServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind('command.env:get', EnvGetCommand::class);
-        $this->app->bind('command.env:set', EnvSetCommand::class);
-        $this->app->bind('command.env:del', EnvDelCommand::class);
-        $this->app->bind('command.env:list', EnvListCommand::class);
-
-        $this->commands([
-            'command.env:get',
-            'command.env:set',
-            'command.env:del',
-            'command.env:list',
-        ]);
+        //
     }
 }
